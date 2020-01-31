9ee99516314e1b7d9ea4268e62e081b88bca9fde
pcesarmf <pedrocesar1410@gmail.com>
2020-01-31 15:16:16 -0300

    Create about route

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Game from './pages/Game';
+import About from './pages/About';
 
 function Routes() {
     return (
@@ -12,6 +13,7 @@ function Routes() {
 				<Route path="/" exact component={ Home } />
 				<Route path="/login" component={ Login } />
                 <Route path="/game" component={ Game } />
+				<Route path="/about" component={ About } />
             </Switch>
         </BrowserRouter>
     );
