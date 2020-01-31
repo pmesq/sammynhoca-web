4032d11ab074c1741cc15d0d53eeef5436b95e8b
pcesarmf <pedrocesar1410@gmail.com>
2020-01-31 14:12:46 -0300

    Create home route

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
+import Home from './pages/Home';
 import Game from './pages/Game';
 
 function Routes() {
     return (
         <BrowserRouter>
             <Switch>
+				<Route path="/" exact component={ Home } />
                 <Route path="/game" component={ Game } />
             </Switch>
         </BrowserRouter>
